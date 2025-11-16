5fb627a55a0bf59cfad7fcb478bd181c587e0e5e
Danesh Patel <danesh_patel@outlook.com>
2025-11-17 01:17:47 +0530

    bump version 2.0.0

--- src/sqlalchemy_jdbcapi/_version.py
+++ src/sqlalchemy_jdbcapi/_version.py
@@ -2,19 +2,20 @@
 # don't change, don't track in version control
 
 __all__ = [
-    "__commit_id__",
     "__version__",
     "__version_tuple__",
-    "commit_id",
     "version",
     "version_tuple",
+    "__commit_id__",
+    "commit_id",
 ]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
+    from typing import Tuple
     from typing import Union
 
-    VERSION_TUPLE = tuple[int | str, ...]
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
 else:
     VERSION_TUPLE = object
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.0.post43+g38e40bcbe.d20251113"
-__version_tuple__ = version_tuple = (0, 0, "post43", "g38e40bcbe.d20251113")
+__version__ = version = '2.0.0'
+__version_tuple__ = version_tuple = (2, 0, 0)
 
-__commit_id__ = commit_id = "g38e40bcbe"
+__commit_id__ = commit_id = 'ga345f5612'
